04e3a02a77dfa9f7c2b9c0d34dbda73595ae4d5e
João Pedro Matias <joao.matias@rebase.com.br>
2021-09-16 10:32:05 -0300

    Transferindo metodo 'event_day_already_passed? do  'Invite' para o 'Event'

--- models/event.rb
+++ models/event.rb
@@ -2,4 +2,8 @@ class Event < ActiveRecord::Base
   has_many :documents, dependent: :destroy
   belongs_to :user, class_name: 'User', foreign_key: 'owner_id'
   validates :name, :local, :owner_id, :description, :start_date, :end_date, presence: true
+
+  def event_day_already_passed?
+    start_date < DateTime.now
+  end
 end
