4b67bb963243bc895ecce76d011e37fbeefc1ac2
João Pedro Matias <joao.matias@rebase.com.br>
2021-09-24 16:12:14 -0300

    Alterações

--- helpers/request_helpers.rb
+++ helpers/request_helpers.rb
@@ -10,26 +10,23 @@ helpers do
     [status(status), body.to_json]
   end
 
-  def user_allowed_to_see_event?
+  def user_allowed_to_see_event!
     user_id = request.env[:user]['id']
     event_exists?
     allowed = accepted_invites.map(&:receiver_id).unshift(event.owner_id).include?(user_id)
 
     halt response_body(403, { error: 'User not allowed' }) unless allowed
-    true
   end
 
-  def user_allowed_to_see_invite?
+  def user_allowed_to_see_invite!
     user_id = request.env[:user]['id']
     allowed = user_id == invite.sender_id || user_id == invite.receiver_id
 
     halt response_body(403, { error: 'User not allowed' }) unless allowed
-    true
   end
 
-  def user_owner_of_the_event?
+  def user_owner_of_the_event!
     halt response_body(403, { error: 'User not allowed' }) unless event.owner_id == request.env[:user]['id']
-    true
   end
 
   def user
@@ -38,12 +35,10 @@ helpers do
 
   def event_exists?
     halt response_body(404, { error: 'Event not found' }) if event.nil?
-    true
   end
 
   def invite_exists?
     halt response_body(404, { error: 'Invite not found' }) if invite.nil?
-    true
   end
 
   def event
@@ -62,14 +57,5 @@ helpers do
 
   def accepted_invites
     Invite.where('event_id = ? and status = ?', @event.id.to_s, '1')
-  end
-
-  def filtred_events?
-    params['start_date'] || params['end_date'] ? true : false
-  end
-
-  def error_on_filtering?(filter_result)
-    halt response_body(400, { error: filter_result }) if filter_result.is_a? String
-    false
   end
 end
