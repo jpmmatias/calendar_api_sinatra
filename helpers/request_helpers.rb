677a4f75e45f61b20fcdc98ab30083412d4c23d7
João Pedro Matias <joao.matias@rebase.com.br>
2021-10-01 09:32:11 -0300

    Adioconando helpers que foram retirados no merge

--- helpers/request_helpers.rb
+++ helpers/request_helpers.rb
@@ -27,4 +27,12 @@ helpers do
   def event_id
     params['event_id'].nil? ? params['id'] : params['event_id']
   end
+
+  def was_provided_filters?
+    params['start_date'] || params['end_date'] ? true : false
+  end
+
+  def error_on_filtering?(results)
+    halt response_body(400, { error: 'Parâmetros de filtros invalidos, tente novamente' }) if results.is_a? String
+  end
 end
