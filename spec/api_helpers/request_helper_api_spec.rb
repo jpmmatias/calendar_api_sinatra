1ea0cc718cbd6c5589691c0380fecc48a9b382e6
João Pedro Matias <joao.matias@rebase.com.br>
2021-10-04 10:44:35 -0300

    WIP teste unitários

--- spec/api_helpers/request_helper_api_spec.rb
+++ spec/api_helpers/request_helper_api_spec.rb
@@ -7,7 +7,7 @@ class TestHelper
   include RequestHelpers
 
   def initialize
-    @params = { 'start_date': '2024-01-01T15:30', 'end_date': '2025-02-01T15:30' }
+    @params = { 'start_date': 'error', 'end_date': 'error' }
   end
 
   def halt(error)
@@ -28,9 +28,9 @@ describe 'Request Helpers' do
   let(:user) { create(:user, email: 'email@gmail.com') }
 
   it 'should return false if was provided search param' do
-    helper.was_provided_filters?.to eq(true)
+    expect(helper.was_provided_filters?).to eq(true)
   end
   it 'error on filtering return an halt' do
-    helper.error_on_filtering?('Error').not_to eq(false)
+    expect(helper.error_on_filtering?('Error')).not_to eq(false)
   end
 end
