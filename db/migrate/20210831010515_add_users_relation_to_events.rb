1f4284adf80f91cbadde668ccbcf0798ef32ad02
João Pedro Matias <joao.matias@rebase.com.br>
2021-10-08 09:51:11 -0300

    Fix bug no databse

--- db/migrate/20210831010515_add_users_relation_to_events.rb
+++ db/migrate/20210831010515_add_users_relation_to_events.rb
@@ -2,7 +2,7 @@ class AddUsersRelationToEvents < ActiveRecord::Migration[6.1]
   def change
     remove_column :events, :owner, :string
     add_column :events, :owner_id, :integer
-    add_reference :events, :owner, references: :user, index: true
+    add_reference :events, :owner, references: :user, index: true, polymorphic: true
     add_foreign_key :events, :users, column: :owner_id
   end
 end
